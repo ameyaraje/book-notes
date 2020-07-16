bfd161bb5bebdff488c3b3f540f02337db5975b2
Ameya Raje <ameyacraje@gmail.com>
2020-07-15 20:58:24 -0400

    added homepage; isbn, author fields

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
+import Header from './Header';
 
 export default class AddBook extends React.Component {
     constructor(props) {
@@ -7,7 +8,9 @@ export default class AddBook extends React.Component {
 
         this.state = {
             title: '',
-            notes: ''
+            notes: '',
+            author: '',
+            isbn: ''
         };
     };
 
@@ -32,36 +35,73 @@ export default class AddBook extends React.Component {
         });
     };
 
+    onAuthorChange = (e) => {
+        this.setState({
+            author: e.target.value
+        });
+    };
+
+    onISBNChange = (e) => {
+        this.setState({
+            isbn: e.target.value
+        });
+    };
+
     render() {
         return (
-            <div className="container">
-                <form onSubmit={this.submitNote}>
-                    <div className="form-group">
-                        <label>Book Title</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="book-title"
-                            placeholder="Enter Book Title Here"
-                            onChange={this.onTitleChange}
-                            value={this.state.title}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Book Notes</label>
-                        <textarea
-                            className="form-control"
-                            id="book-note"
-                            rows="4"
-                            placeholder="Enter Book Notes Here"
-                            onChange={this.onNotesChange}
-                            value={this.state.notes}
-                        >
-                        </textarea>
-                    </div>
-                    <button className="btn btn-primary" type="submit">Submit</button>
-                </form>
-                <hr />
+            <div>
+                <Header />
+                <div className="container">
+                    <form onSubmit={this.submitNote}>
+                        <div className="form-group">
+                            <label>Book Title</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="book-title"
+                                placeholder="Enter Book Title Here"
+                                onChange={this.onTitleChange}
+                                value={this.state.title}
+                            />
+                        </div>
+                        <div class="form-group">
+                            <label>Author</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="author"
+                                placeholder="Enter Author Name Here"
+                                onChange={this.onAuthorChange}
+                                value={this.state.author}
+                            />
+                        </div>
+                        <div class="form-group">
+                            <label>ISBN</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="isbn"
+                                placeholder="Enter ISBN-13 Here"
+                                onChange={this.onISBNChange}
+                                value={this.state.isbn}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Book Notes</label>
+                            <textarea
+                                className="form-control"
+                                id="book-note"
+                                rows="4"
+                                placeholder="Enter Book Notes Here"
+                                onChange={this.onNotesChange}
+                                value={this.state.notes}
+                            >
+                            </textarea>
+                        </div>
+                        <div className="form-group"></div>
+                        <button className="btn btn-primary" type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
         );
     };
